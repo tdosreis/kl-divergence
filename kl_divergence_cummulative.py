d4ca3c5d8007689bc51921bc0ddda1cd40bb0e40
tdosreis <tiagor.reis@gmail.com>
2021-09-12 00:06:35 -0300

    Adds simple scripts illustrating the method

---
+++ kl_divergence_cummulative.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def ecdf(x):
+    x = np.sort(x)
+    u, c = np.unique(x, return_counts=True)
+    n = len(x)
+    y = (np.cumsum(c) - 0.5)/n
+
+    def interpolate_(x_):
+        yinterp = np.interp(x_, u, y, left=0.0, right=1.0)
+        return yinterp
+    return interpolate_
+
+
+def cumulative_kl(x, y, fraction=0.5):
+    dx = np.diff(np.sort(np.unique(x)))
+    dy = np.diff(np.sort(np.unique(y)))
+    ex = np.min(dx)
+    ey = np.min(dy)
+    e = np.min([ex, ey])*fraction
+    n = len(x)
+    P = ecdf(x)
+    Q = ecdf(y)
+    KL = (1./n)*np.sum(np.log((P(x) - P(x-e))/(Q(x) - Q(x-e))))
+    return KL
